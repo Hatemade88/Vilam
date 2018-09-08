f69b517513986f3817299843c5335b9141ad7d03
Andrew Mysnyk <andreymysnyk@ukr.net>
2018-09-08 22:28:29 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+
+gulp.task('sass', function () {
+    return gulp.src('./src/sass/main.scss')
+        .pipe(plumber())
+        .pipe(sass({includePaths: ['src/sass']}))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
